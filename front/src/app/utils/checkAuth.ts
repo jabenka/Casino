d344556681952a4e6a3b6abeedc7c107ff779159
Denzel_Washington <150378576+jabenka@users.noreply.github.com>
2024-11-09 10:39:21 +0300

    Delete front/src/app directory

--- front/src/app/utils/checkAuth.ts
+++
@@ -1,7 +0,0 @@
-// utils/checkAuth.ts
-const checkAuth = (): boolean => {
-    const token = localStorage.getItem('jwtToken');
-    return !!token; // Возвращает true, если токен есть, иначе false
-};
-
-export default checkAuth;
